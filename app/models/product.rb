d90c4b77209f520454bb8b912e9090e0aa20b8e9
Mario <mdavila0482@gmail.com>
2017-04-17 20:59:14 -0500

    Agregando datos desde SEEDS

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-	belongs_to :category inverse_of: :products
+	belongs_to :category, inverse_of: :products
 
 	validates :price, presence: true
 	validates :category, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+Category.destroy_all
+Product.destroy_all
+
+category1 = Category.new(name: 'Category1')
+category1.save
+
+category2 = Category.create(name: 'Category 2')
+category3 = Category.create(name: 'Category 3')
+
+Product.create name: 'Product 1', price: 100, category_id: category1.id
+Product.create name: 'Product 2', price: 150, category: category1
+category1.products << Product.create(name: 'Product 3', price: 150)
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
