6f804422cad9760f327286ee9315d833a257c529
Mario <mdavila0482@gmail.com>
2017-04-17 21:24:43 -0500

    Convirtiendo categorías a mayúsculas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 Category.destroy_all
 Product.destroy_all
 
-category1 = Category.new(name: 'Category1')
-category1.save
+category1 = Category.new(name: 'Category 1')
+puts category1.save
 
-category2 = Category.create(name: 'Category 2')
-category3 = Category.create(name: 'Category 3')
+puts category2 = Category.create(name: 'Category 2')
+puts category3 = Category.create(name: 'Category 3')
 
-Product.create name: 'Product 1', price: 100, category_id: category1.id
-Product.create name: 'Product 2', price: 150, category: category1
-category1.products << Product.create(name: 'Product 3', price: 150)
+puts Product.create name: 'Product 1', price: 100, category_id: category1.id
+puts Product.create name: 'Product 2', price: 150, category: category1
+puts category1.products << Product.create(name: 'Product 3', price: 150)
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
@@ -19,3 +19,21 @@ category1.products << Product.create(name: 'Product 3', price: 150)
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = category2.products.build(name: 'Product 4', price: 200)
+puts product.inspect
+product.save
+puts product.inspect
+product.destroy
+puts ('-------------------------------')
+pro = Product.last
+pro.premium = true
+product.save
+puts pro.inspect
+puts ('-------------------------------')
+
+Category.all.each do |c|
+	c.name = c.name.upcase
+	c.save
+end
+
+Category.all.each { |c| puts c.name }
